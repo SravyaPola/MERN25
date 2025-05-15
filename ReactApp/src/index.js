804ab9ba97d433bb51066750756406ff94b8858f
SravyaPola <sravyapola2000@gmail.com>
2025-05-14 22:05:22 -0700

    Redux Concept

--- ReactApp/src/index.js
+++ ReactApp/src/index.js
@@ -1,13 +1,32 @@
+// console.log("This file is part of webpack configuration and is loaded via bundle.js refered in index.html")
+
+// import React from "react"
+// import ApplicationComponent from "./app/application"
+// import * as ReactDOM from "react-dom/client"
+// import UserComponent from "./app/ApplicationComponents/User/UserComponent"
+// //creating root of the react application where we can load the react app
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// //bootstrapping the react application
+// root.render(
+//     <UserComponent />
+// )
 console.log("This file is part of webpack configuration and is loaded via bundle.js refered in index.html")
 
 import React from "react"
 import ApplicationComponent from "./app/application"
 import * as ReactDOM from "react-dom/client"
-import UserComponent from "./app/ApplicationComponents/User/UserComponent"
+import { Provider } from "react-redux";
+import store from "./app/State/Store";
+
 //creating root of the react application where we can load the react app
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+
 //bootstrapping the react application
 root.render(
-    <UserComponent />
+    //this makes store available as super parent or super props for entire application
+    <Provider store={store}> 
+        <ApplicationComponent/>
+    </Provider>
 )
